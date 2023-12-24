dd96aa1769699559d448bfb688cb9d390f839c0f
rcchen97 <rcchen97@gmail.com>
2023-12-24 17:13:47 -0600

    add letter selector

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import * as hangul from "hangul-js";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
 
 function App() {
   let x = hangul.disassemble("가나다"); // ['ㄱ','ㅏ','ㄴ','ㅏ','ㄷ','ㅏ']
   return (
     <div className="App">
-      {x}
       <Selector />
     </div>
   );
@@ -16,9 +17,15 @@ function App() {
 function Selector() {
   return (
     <>
-  <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"/>
-    
-  </>
+    <h1>Select Starting Consonants</h1>
+    <FormControlLabel control={<Checkbox/>} label="ㄱ (g/k)" />
+    <FormControlLabel control={<Checkbox/>} label="ㄴ (n) " />
+    <FormControlLabel control={<Checkbox/>} label="ㄷ (d or t)" />
+    <h1> Select Vowel(모음) </h1>
+    <FormControlLabel control={<Checkbox/>} label="ㅏ (a)" />
+    <FormControlLabel control={<Checkbox/>} label="ㅓ (eo)" />
+    <FormControlLabel control={<Checkbox/>} label="ㅗ (o)" />
+    </>
   )
 }
 
